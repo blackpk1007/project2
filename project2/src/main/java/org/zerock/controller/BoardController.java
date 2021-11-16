97fa9dd616688175e461e3cde975d1db3cd08b9b
blackpk1007 <blackpk1007@gmail.com>
2021-11-16 20:42:56 +0900

    test ....

--- project2/src/main/java/org/zerock/controller/BoardController.java
+++ project2/src/main/java/org/zerock/controller/BoardController.java
@@ -1,9 +1,15 @@
 package org.zerock.controller;
 
 public class BoardController {
+	
 	 String a = "test";
 	 String b = "test1";
 	 String c = "test2";
 	 String d = "test3";
-	 String f = "test4"; 
+	 String f = "test4";
+	 String g = "test5";
+	 String h = "test6";
+	 String i = "test7";
+	 String j = "test8";
+	 String k = "test9";
 }
