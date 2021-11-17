1409b9aa66249d285e05cedb26f6c602cf826f03
blackpk1007 <blackpk1007@gmail.com>
2021-11-17 23:34:25 +0900

    프젝 시작

--- project2/src/main/java/org/zerock/controller/BoardController.java
+++ project2/src/main/java/org/zerock/controller/BoardController.java
@@ -5,11 +5,4 @@ public class BoardController {
 	 String a = "test";
 	 String b = "test1";
 	 String c = "test2";
-	 String d = "test3";
-	 String f = "test4";
-	 String g = "test5";
-	 String h = "test6";
-	 String i = "test7";
-	 String j = "test8";
-	 String k = "test9";
 }
