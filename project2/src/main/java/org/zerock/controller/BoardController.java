d013893475f0ad532b963bc9b24eec52b7d46c3b
blackpk1007 <blackpk1007@gmail.com>
2021-11-28 23:34:59 +0900

    틀짜기

--- project2/src/main/java/org/zerock/controller/BoardController.java
+++ project2/src/main/java/org/zerock/controller/BoardController.java
@@ -2,9 +2,5 @@ package org.zerock.controller;
 
 public class BoardController {
 	
-	 String a = "test";
-	 String b = "test1";
-	 String c = "test2";
-	 String d = "test3";
-	 String f = "test4";
+	 
 } 
