94673730d24aa970a19365d6c6fe5dc1b7c5c91c
blackpk1007 <blackpk1007@gmail.com>
2021-12-19 23:58:12 +0900

    start...

--- project2/src/main/java/org/zerock/controller/BoardController.java
+++ project2/src/main/java/org/zerock/controller/BoardController.java
@@ -3,11 +3,7 @@ package org.zerock.controller;
 public class BoardController {
 	
 	
-		 String aaaa="";
-		 String aaaa1="1";
-		 String aaaa2="2";
-		 String aaaa3="3";
-		 String aaaa4="4";
+		 String aaaa="Ω√¿€";
 		 
 }
  
